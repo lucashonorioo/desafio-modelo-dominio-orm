9eb23cda0f46548c670547e08cbb98742e464172
Lucas Honorio <lucashonorioo@hotmail.com>
2025-06-13 15:52:48 -0300

    Projeto Criado

---
+++ src/main/java/com/estudo/desafio_orm_sistema_evento/DesafioOrmSistemaEventoApplication.java
@@ -0,0 +1,13 @@
+package com.estudo.desafio_orm_sistema_evento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioOrmSistemaEventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioOrmSistemaEventoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudo/desafio_orm_sistema_evento/DesafioOrmSistemaEventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.desafio_orm_sistema_evento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioOrmSistemaEventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
